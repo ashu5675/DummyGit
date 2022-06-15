8994c0563ab6913c504257280f2075bab2998ef6
Ashutosh Sharma <ashutosh.sharma20@iimb.ac.in>
2022-06-15 13:29:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import time
 import os
 
-setup(name='git1',
+setup(name='AAA_Test',
       packages=['o9dummyfolder'],
       include_package_data=True
       )
